629bc8863e2da7ece42e4ddbd33c3097f1503c55
MANOJ GUJJALA <balu88861@gmail.com>
2023-06-14 23:37:08 +0530

    discoverySErver

--- product-service/src/main/java/com/springboot/productservice/dto/ProductRequest.java
+++ product-service/src/main/java/com/springboot/productservice/dto/ProductRequest.java
@@ -13,6 +13,8 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductRequest {
+
+
     private String name;
     private String description;
     private BigDecimal price;
