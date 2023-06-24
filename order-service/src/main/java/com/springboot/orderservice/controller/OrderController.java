91af0d7569e51431fa645311d4ab7e14934c2605
MANOJ GUJJALA <balu88861@gmail.com>
2023-06-24 21:50:40 +0530

    order-updated

--- order-service/src/main/java/com/springboot/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/springboot/orderservice/controller/OrderController.java
@@ -3,21 +3,38 @@ package com.springboot.orderservice.controller;
 
 import com.springboot.orderservice.dto.OrderRequest;
 import com.springboot.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+//import static org.yaml.snakeyaml.TypeDescription.log;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
     @PostMapping
+    @CircuitBreaker(name = "inventory",fallbackMethod ="fallbackMethod")
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
 
-        return "Order Placed Successfully";
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
--- order-service/src/main/java/com/springboot/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/springboot/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-    public void placeOrder(OrderRequest orderRequest){
+    public String  placeOrder(OrderRequest orderRequest){
         Order order=new Order();
 
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -43,6 +43,7 @@ public class OrderService {
        if (allProductsinstock){
 
       orderRepository.save(order);
+           return "Order Placed Successfully";
        }else{
            throw new IllegalArgumentException("Product is not in Stock please try again later !");
        }
